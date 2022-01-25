d5742a64eebf164c9412e0b3dadb65543cbe2b38
Sambitmohanty <67506332+sambitmohanty272@users.noreply.github.com>
2022-01-25 21:28:00 +0530

    Add files via upload

---
+++ Jan 20/bill.js
@@ -0,0 +1,9 @@
+var library = [{
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
+}];
+for (var i = 0; i < library.length; i++) 
+   {
+   console.log("bill gates reading status is " + library[i].readingStatus);
+}
