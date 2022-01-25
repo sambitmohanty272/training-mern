436b01c801ca561425816bbc951ed447f113502c
Sambitmohanty <67506332+sambitmohanty272@users.noreply.github.com>
2022-01-25 21:25:55 +0530

    Add files via upload

--- Jan 20/index.js
+++ Jan 20/index.js
@@ -1,9 +1,13 @@
-var library = [{
-    author: 'Bill Gates',
-    title: 'The Road Ahead',
-    readingStatus: true
-}];
-for (var i = 0; i < library.length; i++) 
-   {
-   console.log("bill gates reading status is " + library[i].readingStatus);
+var age = prompt("enter your age");
+if(age > 21 && age < 74){
+    alert("you can come in");
+}
+else if(age > 18 && age < 21){
+    alert("Come on in (but no drinking)!");
+}
+else if(age > 75 && age <= 100){
+    alert("Are you sure you want to be here?");
+}
+else{
+    alert("You're too young to be in here!");
 }
