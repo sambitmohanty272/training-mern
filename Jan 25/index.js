b001d0a8bf7f6b669b62a94e9e7837d192f5eeee
Sambitmohanty <67506332+sambitmohanty272@users.noreply.github.com>
2022-01-25 14:41:32 +0530

    Add files via upload

--- Jan 25/index.js
+++ Jan 25/index.js
@@ -1,53 +1,13 @@
-const enterNumber = () => {
-    return new Promise((resolve, reject) => {
-      randN = Math.floor(Math.random() * 6 + 1);
-      userInput = prompt("Enter the Number between 1-6");
-      if (isNaN(userInput)) {
-        reject(alert("Not a valid Number"));
-      }
-      if (userInput === randN) {
-        resolve({
-          point: 2,
-          randN,
-        });
-      } else if (randN === userInput - 1 || randN === userInput + 1) {
-        resolve({
-          point: 1,
-          randN,
-        });
-      } else {
-        resolve({
-          point: 0,
-          randN,
-        });
-      }
-    });
-  };
-  const contGame = () => {
-    return new Promise((resolve) => {
-      if (window.confirm("Do you want to continue?")) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    });
-  };
+async function loadJson(url) { 
+    let response = await fetch(url); 
   
-  const runGame = () => {
-    enterNumber().then((result) => {
-      alert(`Your Point is ${result.point} and Random Number is ${randN}`);
-      contGame()
-        .then((result) => {
-          if (result) {
-            runGame();
-          } else {
-            alert("the game has ended");
-          }
-        })
-        .catch((error) => {
-          alert(error);
-        });
-    });
-  };
-  runGame();
+    if (response.status == 200) {
+      let json = await response.json(); 
+      return json;
+    }
   
+    throw new Error(response.status);
+  }
+  
+  loadJson('no-such-user.json')
+    .catch(alert); // Error: 404
